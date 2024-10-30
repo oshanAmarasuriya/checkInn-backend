ed410730ca58374881ff0c94db3e72bde86cbb50
oshanAmarasuriya <oshansandeep630@gmail.com>
2024-10-30 22:51:47 +0530

    Implement repository for HotelRoomSet

---
+++ src/main/java/com/checkinn/backend/hotel_rooms/HotelRoomSetRepository.java
@@ -0,0 +1,6 @@
+package com.checkinn.backend.hotel_rooms;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRoomSetRepository extends JpaRepository<HotelRoomSet,Long> {
+}
