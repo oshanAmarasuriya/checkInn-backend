f035c9d755cd6629623d8ca0dbfff38444f7da57
oshanAmarasuriya <oshansandeep630@gmail.com>
2025-01-23 15:49:46 +0530

    Add reservation repository

---
+++ src/main/java/com/checkinn/backend/reserv/ReservationRepository.java
@@ -0,0 +1,7 @@
+package com.checkinn.backend.reserv;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+
+}
