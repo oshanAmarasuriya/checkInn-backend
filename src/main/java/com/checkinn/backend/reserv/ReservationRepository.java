c585a8addf630fccfdf484c7e9de865de915d3a8
oshanAmarasuriya <oshansandeep630@gmail.com>
2025-01-23 16:33:11 +0530

    fix validation error when executing custom query

--- src/main/java/com/checkinn/backend/reserv/ReservationRepository.java
+++ src/main/java/com/checkinn/backend/reserv/ReservationRepository.java
@@ -2,10 +2,11 @@ package com.checkinn.backend.reserv;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
-    @Query("select r from Reservation where r.hotelId == :h_id")
-    List<Reservation> findReservByHotelId(Long h_id);
+    @Query("SELECT r FROM Reservation r WHERE r.hotelId = :h_id")
+    List<Reservation> findReservByHotelId(@Param("h_id") Long h_id);
 }
