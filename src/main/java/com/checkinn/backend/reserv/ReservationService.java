0a426d6740f9a4640433f5f3de43594217578303
oshanAmarasuriya <oshansandeep630@gmail.com>
2025-01-23 15:55:14 +0530

    Implement save a reservation

--- src/main/java/com/checkinn/backend/reserv/ReservationService.java
+++ src/main/java/com/checkinn/backend/reserv/ReservationService.java
@@ -1,13 +1,19 @@
 package com.checkinn.backend.reserv;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
 
+    private ReservationRepository repo;
+
+    @Autowired
+    public ReservationService(ReservationRepository repo){this.repo = repo;}
+
     public Reservation createReservation(Reservation reservation) {
         // Business logic to handle reservation placement
-        reservation.setId(1L); // Saving by setting an ID
+        repo.save(reservation);
         return reservation;
     }
 }
